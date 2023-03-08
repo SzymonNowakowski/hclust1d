b6793e5450b0eceb45880ae08e857ea88212c171
SzymonNowakowski <szymon.nowakowski@gmail.com>
2023-03-08 07:21:42 +0100

    a code that compiles: started porting single linkage routine from R to C++

--- src/hclust1d_single.cpp
+++ src/hclust1d_single.cpp
@@ -1,5 +1,6 @@
 #include <Rcpp.h>
 #include <vector>
+#include <assert.h>
 using namespace Rcpp;
 
 // [[Rcpp::plugins("cpp11")]]
@@ -14,35 +15,96 @@ void order(NumericVector & data, std::vector<int> & index) {
          return (data[a] < data[b]);
        }
   );
-
 }
+
+void order(std::vector<double> & data, std::vector<int> & index) {
+  //https://stackoverflow.com/questions/17554242/how-to-obtain-the-index-permutation-after-the-sorting
+
+
+  std::iota(index.begin(), index.end(), 0);
+  sort(index.begin(), index.end(),
+       [&](const int& a, const int& b) {
+         return (data[a] < data[b]);
+       }
+  );
+}
+
 // [[Rcpp::export]]
 List hclust1d_single(NumericVector points) {
 // only single linkage case,
 // which doesn't need a heap because the cluster distances are the same as singleton distances
 
+/*
+ * order_points <- order(points)
+ */
 
-  std::vector<int> order_points(points.size(), 0);
+  auto points_size = points.size();
+  typedef decltype(points_size) R_int;
+  std::vector<int> order_points(points_size, 0);
   order(points, order_points);
 
+// construction of a distance vector
+
 /*
-# construction of a distance vector
-    if (length(points) >= 2) {
+ * if (length(points) >= 2) {
+ */
 
-      count <- length(points)
+    if (points_size >= 2) {
 
-      left_seq <- seq_len(count-2)
-      right_seq <- seq(2, len=count-2)
+  /*
+   *  count <- length(points)
 
-      left_indexes  <- order_points[c(left_seq, count-1)]     # indicates a lower index in a given interval
-      right_indexes <- order_points[c(right_seq, count)]   # indicates a higher index in a given interval
-      distances <- points[right_indexes] - points[left_indexes]      # lengths of an interval
+   * left_seq <- seq_len(count-2)
+   * right_seq <- seq(2, len=count-2)
+   */
 
-      left_ids <- c(0, left_seq)    # 0 means "no id to the left"
-      right_ids <- c(right_seq, 0)  # 0 means "no id to the right"
-      left_merges  <- -left_indexes      # minus as in original hclust implementation
-      right_merges <- -right_indexes     # to indicate merge with singletons
+      auto left_seq = [&](R_int i) {
+        // !!! represents R-ish c(left_seq, count-1)
+        //input: indexes from 0 to points_size - 2, count: points_size - 1
+        //output: indexes from 0 to points_size -2
+        assert(i >= 0 and i < points_size - 1);
+        return i;
+      };
 
+      auto right_seq = [&](R_int i) {
+        // !!! represents R-ish c(right_seq, count)
+        //input: indexes from 0 to points_size - 2, count: points_size - 1
+        //output: indexes from 1 to points_size - 1
+        assert(i >= 0 and i < points_size - 1);
+        return i + 1;
+      };
+
+  /* left_indexes  <- order_points[c(left_seq, count-1)]     # indicates a lower index in a given interval
+   * right_indexes <- order_points[c(right_seq, count)]   # indicates a higher index in a given interval
+   * distances <- points[right_indexes] - points[left_indexes]      # lengths of an interval
+   */
+      auto left_indexes = [&](R_int i) {
+        //input: indexes from 0 to points_size - 2, count: points_size - 1
+        assert(i >= 0 and i < points_size - 1);
+        return order_points[left_seq(i)];
+      };
+
+      auto right_indexes = [&](R_int i) {
+        //input: indexes from 0 to points_size - 2, count: points_size - 1
+        assert(i >= 0 and i < points_size - 1);
+        return order_points[right_seq(i)];
+      };
+
+      std::vector<double> distances;
+      for (R_int i = 0; i < points_size - 1; i++)
+        distances.push_back(points[right_indexes(i)] - points[left_indexes(i)]);
+
+
+  /*
+   * left_ids <- c(0, left_seq)    # 0 means "no id to the left"
+   * right_ids <- c(right_seq, 0)  # 0 means "no id to the right"
+   * left_merges  <- -left_indexes      # minus as in original hclust implementation
+   * right_merges <- -right_indexes     # to indicate merge with singletons
+   */
+
+
+    }  //end of the if statement
+/*
 
     } else {   #at most 1 value in v
       distances <- numeric(0)
@@ -53,8 +115,8 @@ List hclust1d_single(NumericVector points) {
       id_seq <- seq_along(distances)   #we compute the id sequence
 
 
-      merge <- matrix(nrow=count - 1, ncol=2)
-      height <- rep(0, count - 1)
+      merge <- matrix(nrow=points_size - 1, ncol=2)
+      height <- rep(0, points_size - 1)
 
       for (stage in id_seq) {   #id_seq == seq_along(distances)
 #to be precise: from the semantic POV, this line should read
