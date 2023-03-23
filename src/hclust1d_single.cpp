5be603b67fe4d5d29bea45c330bee08e06b621cc
unknown <szymon.nowakowski@gmail.com>
2023-03-23 17:20:45 +0100

    a working single linkage version

--- src/hclust1d_single.cpp
+++ src/hclust1d_single.cpp
@@ -50,86 +50,84 @@ List hclust1d_single(NumericVector points) {
  * if (length(points) >= 2) {
  */
 
-  if (points_size == 0)
-    stop("no points");
+  if (points_size < 2)
+    stop("at least two objects are needed to analyse clusters");
+
+/*
+*  count <- length(points)
+
+* left_seq <- seq_len(count-2)
+* right_seq <- seq(2, len=count-2)
+*/
+  //the sequence indexed by the numbers of intervals (there are points_size - 1 intervals)
+  //and returning an index of a left point in each interval (as if they were ordered, but they are not)
+  auto left_seq = [&](int i) {
+    // !!! represents R-ish c(left_seq, count-1)
+    //input: indexes from 0 to points_size - 2, count: points_size - 1
+    //output: indexes from 0 to points_size -2
+    assert(i >= 0 and i < points_size - 1);
+    return i;
+  };
+
+  //the sequence indexed by the numbers of intervals (there are points_size - 1 intervals)
+  //and returning an index of a right point in each interval (as if they were ordered, but they are not)
+  auto right_seq = [&](int i) {
+    // !!! represents R-ish c(right_seq, count)
+    //input: indexes from 0 to points_size - 2, count: points_size - 1
+    //output: indexes from 1 to points_size - 1
+    assert(i >= 0 and i < points_size - 1);
+    return i + 1;
+  };
+
+/* left_indexes  <- order_points[c(left_seq, count-1)]     # indicates a lower index in a given interval
+* right_indexes <- order_points[c(right_seq, count)]   # indicates a higher index in a given interval
+* distances <- points[right_indexes] - points[left_indexes]      # lengths of an interval
+*/
+  //the sequence indexed by the numbers of intervals (there are points_size - 1 intervals)
+  //and returning an index of a left point in each interval
+  auto left_indexes = [&](int i) {
+    //input: indexes from 0 to points_size - 2, count: points_size - 1
+    assert(i >= 0 and i < points_size - 1);
+    return order_points[left_seq(i)];
+  };
+
+  //the sequence indexed by the numbers of intervals (there are points_size - 1 intervals)
+  //and returning an index of a right point in each interval
+  auto right_indexes = [&](int i) {
+    //input: indexes from 0 to points_size - 2, count: points_size - 1
+    assert(i >= 0 and i < points_size - 1);
+    return order_points[right_seq(i)];
+  };
 
   std::vector<double> distances;
+  //the sequence of distances within intervals (there are points_size - 1 intervals)
+  for (int i = 0; i < points_size - 1; i++)
+    distances.push_back(points[right_indexes(i)] - points[left_indexes(i)]);
+
+
+/*
+* left_ids <- c(0, left_seq)    # 0 means "no id to the left"
+* right_ids <- c(right_seq, 0)  # 0 means "no id to the right"
+* left_merges  <- -left_indexes      # minus as in original hclust implementation
+* right_merges <- -right_indexes     # to indicate merge with singletons
+*/
+
   std::vector<int> interval_left_ids(points_size-1);
+  std::iota(interval_left_ids.begin(), interval_left_ids.end(), -1);
+              // in C++: -1 means "no id to the left"
+
   std::vector<int> interval_right_ids(points_size-2);
+  std::iota(interval_right_ids.begin(), interval_right_ids.end(), 1);
+  interval_right_ids.push_back(-1); // in C++: -1 means "no id to the right"
+
   std::vector<int> left_merges(points_size - 1);
   std::vector<int> right_merges(points_size - 1);
-  if (points_size >= 2) {
-
-  /*
-   *  count <- length(points)
-
-   * left_seq <- seq_len(count-2)
-   * right_seq <- seq(2, len=count-2)
-   */
-      //the sequence indexed by the numbers of intervals (there are points_size - 1 intervals)
-      //and returning an index of a left point in each interval (as if they were ordered, but they are not)
-      auto left_seq = [&](int i) {
-        // !!! represents R-ish c(left_seq, count-1)
-        //input: indexes from 0 to points_size - 2, count: points_size - 1
-        //output: indexes from 0 to points_size -2
-        assert(i >= 0 and i < points_size - 1);
-        return i;
-      };
-
-      //the sequence indexed by the numbers of intervals (there are points_size - 1 intervals)
-      //and returning an index of a right point in each interval (as if they were ordered, but they are not)
-      auto right_seq = [&](int i) {
-        // !!! represents R-ish c(right_seq, count)
-        //input: indexes from 0 to points_size - 2, count: points_size - 1
-        //output: indexes from 1 to points_size - 1
-        assert(i >= 0 and i < points_size - 1);
-        return i + 1;
-      };
-
-  /* left_indexes  <- order_points[c(left_seq, count-1)]     # indicates a lower index in a given interval
-   * right_indexes <- order_points[c(right_seq, count)]   # indicates a higher index in a given interval
-   * distances <- points[right_indexes] - points[left_indexes]      # lengths of an interval
-   */
-      //the sequence indexed by the numbers of intervals (there are points_size - 1 intervals)
-      //and returning an index of a left point in each interval
-      auto left_indexes = [&](int i) {
-        //input: indexes from 0 to points_size - 2, count: points_size - 1
-        assert(i >= 0 and i < points_size - 1);
-        return order_points[left_seq(i)];
-      };
-
-      //the sequence indexed by the numbers of intervals (there are points_size - 1 intervals)
-      //and returning an index of a right point in each interval
-      auto right_indexes = [&](int i) {
-        //input: indexes from 0 to points_size - 2, count: points_size - 1
-        assert(i >= 0 and i < points_size - 1);
-        return order_points[right_seq(i)];
-      };
-
-      //the sequence of distances within intervals (there are points_size - 1 intervals)
-      for (int i = 0; i < points_size - 1; i++)
-        distances.push_back(points[right_indexes(i)] - points[left_indexes(i)]);
+  for (int i=0; i<points_size - 1; i++) {
+    left_merges[i] = -left_indexes(i) - 1;
+    right_merges[i] = -right_indexes(i) - 1;
+  }
 
 
-  /*
-   * left_ids <- c(0, left_seq)    # 0 means "no id to the left"
-   * right_ids <- c(right_seq, 0)  # 0 means "no id to the right"
-   * left_merges  <- -left_indexes      # minus as in original hclust implementation
-   * right_merges <- -right_indexes     # to indicate merge with singletons
-   */
-
-      std::iota(interval_left_ids.begin(), interval_left_ids.end(), -1);
-                  // in C++: -1 means "no id to the left"
-
-      std::iota(interval_right_ids.begin(), interval_right_ids.end(), 0);
-      interval_right_ids.push_back(-1); // in C++: -1 means "no id to the right"
-
-      for (int i=0; i<points_size - 1; i++) {
-        left_merges[i] = -left_indexes(i);
-        right_merges[i] = -right_indexes(i);
-      }
-
-    }  //end of the if statement
 
 /*
  *  } else {   #at most 1 value in v
