6729a9f518878703e676109186921297734203b9
SzymonNowakowski <szymon.nowakowski@gmail.com>
2023-04-07 23:43:41 +0200

    rearranging order of functions for improved code readability

--- src/heap.cpp
+++ src/heap.cpp
@@ -4,7 +4,7 @@
 /*
  *                          a custom heap implementation
  *
- * in addition to std::heap it provides the following functionalities:
+ * in addition to std::heap-like functions it provides the following functionalities:
  *
  * * reverse_lookup for updating key by id
  * * ability to heapify both down and up the tree for updating
@@ -12,15 +12,32 @@
  *
  */
 
-
-
 //first some housekeeping functions enabling us access on a vector via tree relations
 //with 0-based indexing
 int left(int i) { return 2*i+1; }
 int right(int i) { return 2*i+2; }
 int parent(int i) { return (i-1)/2; }
 
-void remove_all(struct heap & h) { h.keys.clear(); h.ids.clear(); }
+struct heap init_heap(std::vector<double> keys) {
+  //pass by value the keys because they get assigned and rearanged
+  //the ids associated with keys are 0 .. keys.size() - 1
+  //please note, that the returned heap may have the ids field rearranged and not in this sequence
+
+  struct heap h;
+  h.keys = keys;
+  h.ids = std::vector<int>(h.keys.size());
+  std::iota(h.ids.begin(), h.ids.end(), 0);
+  h.reverse_lookup = std::vector<int>(h.keys.size());
+  std::iota(h.reverse_lookup.begin(), h.reverse_lookup.end(), 0);
+
+  for (int i = parent(size(h) - 1); i>=0; i--)   //parent of the last element is the first one
+    heapify_down(h, i);                                 //which may need a rebuild
+  //it is safe, as for cnt==0, parent==-1
+  //               for cnt==1, parent==-1
+  //trigerring an empty loop
+  return h;
+}
+
 int size(struct heap & h) { return h.keys.size(); }
 bool is_empty(struct heap & h) { return size(h) == 0; }
 
@@ -30,6 +47,58 @@ std::pair<double, int> read_minimum(struct heap & h) {
 
  return std::pair<double, int>(0.0, 0);   //for reading minimum of an empty heap
 }
+
+std::pair<double, int> remove_minimum(struct heap & h) {
+  std::pair<double, int> r = read_minimum(h);
+  if (size(h) <= 1)
+    remove_all(h);
+  else {
+    switch_node(h, 0, size(h)-1);
+    h.keys.pop_back();
+    h.ids.pop_back();
+
+    heapify_down(h, 0);
+  }
+  return r;
+}
+
+void remove_all(struct heap & h) { h.keys.clear(); h.ids.clear(); }
+
+int insert(struct heap & h, double key) {
+  // returning the id of the inserted key
+
+  h.keys.push_back(key);                           //push_back is safe reallocation-wise
+  h.reverse_lookup.push_back(h.keys.size()-1);
+  h.ids.push_back(h.reverse_lookup.size()-1);
+
+  heapify_up(h, size(h) -1);
+
+  return h.reverse_lookup.size()-1;
+}
+
+double read_key_by_id(struct heap & h, int id) {
+  int index = h.reverse_lookup[id];    // this is the spot when we come to need the reverse_lookup array
+  return h.keys[index];
+}
+
+void update_key_by_id(struct heap & h, int id, double new_key) {
+  int index = h.reverse_lookup[id];    // this is the spot when we come to need the reverse_lookup array
+  h.keys[index] = new_key;
+
+  // the index's new key either got smaller than his parent's
+  // or got larger than in one of his sons
+  // or everything is OK.
+  //
+  // Both corrections (in the up direction or in the down direction) will be attempted
+  // but only *at most* one of them will proceed after the initial checks
+
+  heapify_down(h, index);
+  heapify_up(h, index);
+}
+
+//////////////////////////////////////////////
+//////////// TECHNICALITIES //////////////////
+//////////////////////////////////////////////
 
 void switch_node(struct heap & h, int i, int j) {
           // switches nodes i and j
@@ -54,7 +123,6 @@ void switch_node(struct heap & h, int i, int j) {
   h.reverse_lookup[id_j] = i;
 }
 
-
 void heapify_up(struct heap & h, int i) {
 // the assumption is that i is the proper heap
 // and that the parent of i is smaller than his sons
@@ -70,19 +138,6 @@ void heapify_up(struct heap & h, int i) {
     }
   }
 }
-
-int insert(struct heap & h, double key) {
-// returning the id of the inserted key
-
-  h.keys.push_back(key);                           //push_back is safe reallocation-wise
-  h.reverse_lookup.push_back(h.keys.size()-1);
-  h.ids.push_back(h.reverse_lookup.size()-1);
-
-  heapify_up(h, size(h) -1);
-
-  return h.reverse_lookup.size()-1;
-}
-
 
 void heapify_down(struct heap & h, int i) {
 // the assumption is that both i's sons are proper heaps
@@ -106,60 +161,6 @@ void heapify_down(struct heap & h, int i) {
     switch_node(h, i, minimal);
     heapify_down(h, minimal);
   }
-}
-
-std::pair<double, int> remove_minimum(struct heap & h) {
-  std::pair<double, int> r = read_minimum(h);
-  if (size(h) <= 1)
-    remove_all(h);
-  else {
-    switch_node(h, 0, size(h)-1);
-    h.keys.pop_back();
-    h.ids.pop_back();
-
-    heapify_down(h, 0);
-  }
-  return r;
-}
-
-struct heap init_heap(std::vector<double> keys) {
-//pass by value the keys because they get assigned and rearanged
-//the ids associated with keys are 0 .. keys.size() - 1
-//please note, that the returned heap may have the ids field rearranged and not in this sequence
-
-  struct heap h;
-  h.keys = keys;
-  h.ids = std::vector<int>(h.keys.size());
-  std::iota(h.ids.begin(), h.ids.end(), 0);
-  h.reverse_lookup = std::vector<int>(h.keys.size());
-  std::iota(h.reverse_lookup.begin(), h.reverse_lookup.end(), 0);
-
-  for (int i = parent(size(h) - 1); i>=0; i--)   //parent of the last element is the first one
-    heapify_down(h, i);                                 //which may need a rebuild
-                                                        //it is safe, as for cnt==0, parent==-1
-                                                        //               for cnt==1, parent==-1
-                                                        //trigerring an empty loop
-  return h;
-}
-
-double read_key_by_id(struct heap & h, int id) {
-  int index = h.reverse_lookup[id];    // this is the spot when we come to need the reverse_lookup array
-  return h.keys[index];
-}
-
-void update_key_by_id(struct heap & h, int id, double new_key) {
-  int index = h.reverse_lookup[id];    // this is the spot when we come to need the reverse_lookup array
-  h.keys[index] = new_key;
-
-// the index's new key either got smaller than his parent's
-// or got larger than in one of his sons
-// or everything is OK.
-//
-// Both corrections (in the up direction or in the down direction) will be attempted
-// but only *at most* one of them will proceed after the initial checks
-
-  heapify_down(h, index);
-  heapify_up(h, index);
 }
 
 // [[Rcpp::export()]]
