d709fe9f57c9905b383f301ccc90b4000f264bc6
SzymonNowakowski <szymon.nowakowski@gmail.com>
2023-04-13 22:32:55 +0200

    fixing a missing sqrt call

--- src/sqrt.cpp
+++ src/sqrt.cpp
@@ -9,6 +9,7 @@ NumericVector sqrt(NumericVector & squared_distances) {
   for (int i = 1; i < squared_distances.size(); i++) {
     if (squared_distances[i] < 0)
       stop("A negative value found in a squared distance matrix");
+    squared_distances[i] = std::sqrt(squared_distances[i]);
   }
   return squared_distances;
 }
